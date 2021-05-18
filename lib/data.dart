90548f4b2467f19390fe7791893d55daff9049c1
aakzsh <69726390+aakzsh@users.noreply.github.com>
2021-05-18 10:47:41 +0530

    add city data

--- lib/data.dart
+++ lib/data.dart
@@ -10,371 +10,375 @@ var paris = [
   "France",
   "11.7 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
+  "223 USD per day",
   "History, culture, food and fashion in one picture-perfect city."
 ];
 var aires = [
   "Beunos Aires",
   "Argentina",
+  "15 C",
+  "lol",
+  "45 USD per day",
+  "Buenos Aires is in the midst of a tourism boom, according to the World Travel & Tourism Council",
 ];
 var athens = [
   "Athens",
   "Greece",
-  "11.7 C",
+  "25 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "87 USD per day",
+  "Many of Classical civilization's intellectual and artistic ideas originated there, and the city is generally considered to be the birthplace of Western civilization."
 ];
 var auckland = [
   "Auckland",
-  "France",
-  "11.7 C",
+  "New Zealand",
+  "10 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "225 USD per day",
+  "Auckland sprawls over volcanic hills and around twin harbours, offering an exhilarating mix of natural wonders and urban adventures."
 ];
 var bali = [
   "Bali",
-  "France",
-  "11.7 C",
+  "Indonesia",
+  "10 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "65 USD per day",
+  "Captivating Cultural Scene. Bali's beauty goes way deeper than its beautiful appearances."
 ];
 var bangkok = [
   "Bangkok",
-  "France",
-  "11.7 C",
+  "Thailand",
+  "20 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "63.60 USD per day",
+  "The city of Bangkok has so much to offer along the lines of history, culture, traditions and architectural beauty"
 ];
 var bath = [
   "Bath",
-  "France",
-  "11.7 C",
+  "England",
+  "9.6 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "98 USD per day",
+  "Bath's long history as a spa centre, which began as a result of the amazing, natural hot springs here, is as important today as it ever was."
 ];
 var barcelona = [
   "Barcelona",
-  "France",
-  "11.7 C",
+  "Spain",
+  "15.3 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "104 USD per day",
+  "Gaudi's impressive architecture, which includes the Parc Güell and the still unfinished Sagrada Família."
 ];
 var beijing = [
   "Beijing",
-  "France",
-  "11.7 C",
+  "China",
+  "26 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "56 USD per day",
+  "Beijing holds many magnificent sights that set national or even world records."
 ];
 var berlin = [
   "Berlin",
-  "France",
-  "11.7 C",
+  "Germany",
+  "10 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "124 USD per day",
+  "Berlin is a city celebrated for its freedom, creativity and grungy cool attitude."
 ];
 var bruges = [
   "Bruges",
-  "France",
-  "11.7 C",
+  "Belgium",
+  "12.3 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "115 USD",
+  "Get lost in the city. The whole city centre of Bruges is listed as UNESCO cultural heritage."
 ];
 var cairo = [
-  "Paris",
-  "France",
-  "11.7 C",
+  "Cairo",
+  "Egypt",
+  "30 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "28 USD per day",
+  "One of the biggest things that attracts millions of tourists to Cairo are the pyramids."
 ];
 var capetown = [
   "Capetown",
-  "France",
-  "11.7 C",
+  "South Africa",
+  "24.6 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "102 USD per day",
+  "It's beautiful. Cape Town is one of the most beautiful cities in the world."
 ];
 var casablanca = [
   "Casablanca",
-  "France",
-  "11.7 C",
+  "Morocco",
+  "17.5 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "54 USD per day",
+  "Feast Your Eyes on One of Morocco's Most Beautiful Mosques."
 ];
 var copenhagen = [
   "Copenhagen",
-  "France",
-  "11.7 C",
+  "Denmark",
+  "11.5 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "194 USD per day",
+  "There Are Highways For Bikes. If ever a city was made to be enjoyed on a bike then this is it."
 ];
 var dublin = [
   "Dublin",
-  "France",
-  "11.7 C",
+  "Ireland",
+  "16 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "136 USD per day",
+  "Dublin has the Best Pubs."
 ];
 var dubai = [
   "Dubai",
-  "France",
-  "11.7 C",
+  "UAE",
+  "25 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "192.5 USD per day",
+  "Dubai is undoubtedly one of the most unique cities in the world."
 ];
 var edinburgh = [
   "Edinburgh",
-  "France",
-  "11.7 C",
+  "Scotland",
+  "13.6 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "158 USD per day",
+  "It's world's leading festival city."
 ];
 var florence = [
   "Florence",
-  "France",
-  "11.7 C",
+  "Italy",
+  "16.2 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "141 USD per day",
+  "For the Art and Architecture. Walking around the streets of Florence is like being whisked back in time."
 ];
 var helsinki = [
   "Helsinki",
-  "France",
-  "11.7 C",
+  "Finland",
+  "14.3 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "136 USD per day",
+  "Sea all around you. You can sense the proximity of the sea everywhere, as the city has over a hundred kilometres of shoreline and around 300 islands in its archipelago."
 ];
 var hongkong = [
   "HongKong",
-  "France",
-  "11.7 C",
+  "HK",
+  "19.3 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "140 USD per day",
+  "It's the city of skyscrapers."
 ];
 var honolulu = [
-  "Paris",
-  "France",
-  "11.7 C",
+  "Honolulu",
+  "Hawaii",
+  "20 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "196 USD per day",
+  "The world's largest open-air shopping center."
 ];
 var istanbul = [
   "Istanbul",
-  "France",
-  "11.7 C",
+  "Turkey",
+  "23 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "37.16 USD per day",
+  "The city of four empires."
 ];
 var jerusalem = [
   "Jerusalem",
-  "France",
-  "11.7 C",
+  "Israel",
+  "24 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "90 USD per day",
+  "A rich mix of old and new. Jerusalem has an intricate history as a cornerstone to three of the world's religions – Judaism, Christianity and Islam."
 ];
 var krakow = [
   "Krakow",
-  "France",
-  "11.7 C",
+  "Poland",
+  "12 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "63.08 USD per day",
+  "Krakow is an easy city to love. It's steeped in rich history, both glorious and infamous."
 ];
 var lisbon = [
   "Lisbon",
-  "France",
-  "11.7 C",
+  "Portugal",
+  "15 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "117 USD per day",
+  "The climate is perfect. Lisbon's geographic position means great weather all year round."
 ];
 var london = [
   "London",
-  "France",
-  "11.7 C",
+  "England",
+  "11.3 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "203 USD per day",
+  "London has been honed into a genuinely incredible city through its 2,000-year history."
 ];
 var losangeles = [
   "Los Angeles",
-  "France",
-  "11.7 C",
+  "USA",
+  "20 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "190 USD per day",
+  "The Beaches. Los Angeles is home to some of the USA's best beaches and each one has its own unique character."
 ];
 var madrid = [
   "Madrid",
-  "France",
-  "11.7 C",
+  "Spain",
+  "15 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "118 USD per day",
+  "The city has one of Europe's finest collections of Spanish and Latin American art, spread throughout its museums and galleries."
 ];
 var mumbai = [
   "Mumbai",
-  "France",
-  "11.7 C",
+  "India",
+  "26 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "60 USD per day",
+  "Mumbai is a mix of iconic old-world charm architecture, strikingly modern high rises, cultural and traditional structures, and whatnot."
 ];
 var munich = [
   "Munich",
-  "France",
-  "11.7 C",
+  "Germany",
+  "14.2 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "132 USD per day",
+  "The best beer gardens."
 ];
 var nyc = [
   "New York",
-  "France",
-  "11.7 C",
+  "USA",
+  "13.2 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "239 USD per day",
+  "The Buildings. There's a reason you'll see so many people looking up when you visit New York!"
 ];
 var osaka = [
   "Osaka",
-  "France",
-  "11.7 C",
+  "Japan",
+  "21 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "80 USD per day",
+  "Food worth going bankrupt for."
 ];
 var oslo = [
   "Oslo",
-  "France",
-  "11.7 C",
+  "Norway",
+  "9 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "50 USD per day",
+  "Attractive & Accessible Location. Oslo is easily reached by train, bus, ferry plane or car."
 ];
 var qatar = [
   "Qatar",
-  "France",
-  "11.7 C",
+  "Qatar",
+  "26 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "250 USD per day",
+  "There's a remarkable blend of tradition and modernity."
 ];
 var rio = [
   "Rio De Jeneiro",
-  "France",
-  "11.7 C",
+  "Brazil",
+  "23 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "77 USD per day",
+  "The rocky peaks afford great viewpoints from Christ the Redeemer and the Sugarloaf mountain."
 ];
 var rome = [
   "Rome",
-  "France",
-  "11.7 C",
+  "Italy",
+  "15 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "388 USD per day",
+  "Art and culture. If not the richest, Italy has one of the richest cultural heritages in the world."
 ];
 var seattle = [
   "Seattle",
-  "France",
-  "11.7 C",
+  "USA",
+  "19 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "156 USD per day",
+  "You have to try the coffee shops. The original Starbucks on Pike Place – Seattle is the home of coffee culture."
 ];
 var seoul = [
   "Seoul",
-  "France",
-  "11.7 C",
+  "South Korea",
+  "14.6 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "103 USD per day",
+  "Dress to impress. Seoul has emerged as one of the world's most fashion forward cities."
 ];
 var shanghai = [
   "Shanghai",
-  "France",
-  "11.7 C",
+  "China",
+  "23 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "42 USD per day",
+  "China's Largest and Richest City. Shanghai has a population of more than 20 million."
 ];
 var singapore = [
   "Singapore",
-  "France",
-  "11.7 C",
+  "Singapore",
+  "16 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "123 USD per day",
+  "Singapore actually has some pretty good beaches."
 ];
 var tokyo = [
   "Tokyo",
-  "France",
-  "11.7 C",
+  "Japan",
+  "14 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "179 USD per day",
+  "The world's most sophisticated railways."
 ];
 var toronto = [
   "Toronto",
-  "France",
-  "11.7 C",
+  "Canada",
+  "14 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "185 USD per day",
+  "It has plenty of destinations to tick off your bucket list."
 ];
 var vegas = [
   "Vegas",
-  "France",
-  "11.7 C",
+  "USA",
+  "19 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "255 USD per day",
+  "The weather. It gets hot in Las Vegas – really hot."
 ];
 var venice = [
   "Venice",
-  "France",
-  "11.7 C",
+  "Italy",
+  "12 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  "220 USD per day",
+  "City of canals. Venice — known also as the 'Floating City' — has no roads accessible by car."
 ];
 var vienna = [
   "Vienna",
-  "France",
-  "11.7 C",
+  "Austria",
+  "12 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
-  "History, culture, food and fashion in one picture-perfect city."
+  " 142 USD per day",
+  "Visit Vienna for interesting history and beautiful landmarks."
 ];
 var delhi = [
   "Delhi",
   "India",
-  "11.7 C",
+  "27 C",
   "https://i.ibb.co/DYcSJDy/paris.png",
-  "budget",
+  "34 USD per day",
   "Dildaar Dilli"
 ];
 
